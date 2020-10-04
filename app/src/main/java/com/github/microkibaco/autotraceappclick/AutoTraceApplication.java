a1e3287317129a8b0abcc0da5d4b1d4adc6dd494
杨正友 <zhengyouyang.yzy@nonolive.com>
2020-10-05 00:25:29 +0800

    点击事件信息查询失败,问题有待定位

---
+++ app/src/main/java/com/github/microkibaco/autotraceappclick/AutoTraceApplication.java
@@ -0,0 +1,10 @@
+package com.github.microkibaco.autotraceappclick;
+
+/**
+ * @author 杨正友(小木箱)于 2020/10/5 00 08 创建
+ * @Email: yzy569015640@gmail.com
+ * @Tel: 18390833563
+ * @function description:
+ */
+class AutoTraceApplication {
+}
---
+++ track_sdk/src/main/java/com/github/microkibaco/track_sdk/ITrackClickEvent.java
@@ -0,0 +1,10 @@
+package com.github.microkibaco.track_sdk;
+
+/**
+ * @author 杨正友(小木箱)于 2020/10/4 23 49 创建
+ * @Email: yzy569015640@gmail.com
+ * @Tel: 18390833563
+ * @function description:
+ */
+interface ITrackClickEvent {
+}
